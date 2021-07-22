327ccc59d92ec6297b3826ff518bd3fd18879518
Tim Marchant <fromtimwithcode@gmail.com>
2021-07-22 14:59:04 -0500

    Completed Task 1

--- index.js
+++ index.js
@@ -42,7 +42,23 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+  eat(food) {
+    if(this.stomach.length < 10) {
+      this.stomach.push(food);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -60,7 +76,22 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    if((this.tank/this.milerPerGallon) - (distance/this.milesPerGallon) > 0) {
+      this.odometer += distance;
+      this.tank -= (distance * this.milesPerGallon);
+    }
+  }
 }
 
 /*
